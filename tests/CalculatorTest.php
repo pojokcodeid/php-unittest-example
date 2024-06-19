8ea7fddbe67d8b80b206073edc44001bbd0f3b57
asep.komarudin <asep.komarudin@dataon.com>
2024-06-19 07:00:37 +0700

    first commit

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class CalculatorTest extends \PHPUnit\Framework\TestCase
+{
+  public function testAdd()
+  {
+    $calculator = new App\Calculator;
+    $result = $calculator->add(20, 5);
+    $this->assertEquals(25, $result);
+  }
+  public function testSubstract()
+  {
+    $calculator = new App\Calculator;
+    $result = $calculator->subtract(20, 5);
+    $this->assertEquals(15, $result);
+  }
+}
